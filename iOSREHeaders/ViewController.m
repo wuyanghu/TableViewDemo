fe726284d796086b1afb7a802d2a623e8004bb21
wupeng <823105162@qq.com>
2019-07-02 20:24:03 +0800

    iOS headers

---
+++ iOSREHeaders/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOSREHeaders
+//
+//  Created by ruantong on 2019/7/2.
+//  Copyright © 2019 wupeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ iOSREHeaders/traveDir.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import re
+import json
+import sys
+import fileinput
+import shelve
+
+str1='\xe6\x88\x91'
+print unicode(str1,'utf-8')
+
+
+str2 = '\u4fe1\u7528\u5361\u8fd8\u6b3e'
+print str2.decode('unicode_escape')
